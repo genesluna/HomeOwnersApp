9f194eb341a24b4a82488334d4ac063f23d3a2e6
Genes Luna <genesluna@gmail.com>
2023-08-24 20:00:54 -0300

    Changed data types

--- Models/HomeOwners.cs
+++ Models/HomeOwners.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeOwnersApp.Models;
 
@@ -6,11 +7,17 @@ public class HomeOwners
 {
     public Guid Id { get; set; }
     [Required]
+    [Column(TypeName = "nvarchar(200)")]
     public string? Name { get; set; }
+
     [Required]
+    [Column(TypeName = "nvarchar(30)")]
     public string? PhoneNumber { get; set; }
+
     [Required]
     public int Age { get; set; }
+
     [Required]
+    [Column(TypeName = "nvarchar(200)")]
     public string? Site { get; set; }
 }
