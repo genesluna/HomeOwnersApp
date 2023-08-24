40b81c7166d01abfaa46950d3754097d473f8c37
Genes Luna <genesluna@gmail.com>
2023-08-24 16:44:01 -0300

    Added home owner model

---
+++ Models/HomeOwners.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeOwnersApp.Models;
+
+public class HomeOwners
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? PhoneNumber { get; set; }
+    [Required]
+    public int Age { get; set; }
+    [Required]
+    public string? Site { get; set; }
+}
