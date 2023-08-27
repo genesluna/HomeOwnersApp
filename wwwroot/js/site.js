fcd288105d1228fccf4dab6e6c2cf3bed2e4d8df
Genes Luna <genesluna@gmail.com>
2023-08-27 14:47:10 -0300

    Added datatables, tooltips and tabs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Data Tables
+$(document).ready(function () {
+    $('#OwnersTable').DataTable({
+        "responsive": true,
+        "language": {
+            "url": "/datatables/plug-ins/i18n/pt-BR.json"
+        },
+    });
+});
 
-// Write your JavaScript code.
+// Bootstrap tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Bootstrap tabs
+$(function () {
+    const currentPage = location.pathname;
+    $('.nav-tabs li a').each(function () {
+        const $this = $(this);
+        if (currentPage.toLowerCase().indexOf($this.attr('href').toLowerCase()) !== -1) {
+            $this.addClass('active');
+        }
+    })
+})
